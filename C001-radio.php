62be8c1d86618b9097b03fa7d0403ad7a050fb84
k-sasaki123 <ky211593@gmail.com>
2020-03-27 09:33:49 +0900

    解答見本

--- C001-radio.php
+++ C001-radio.php
@@ -1,45 +1,59 @@
 <form name="radioB">
-カナダの首都は？<br>
-<input type="radio" name="Q1">オタワ<br>
-<input type="radio" name="Q1">トロント<br>
-<input type="radio" name="Q1">モントリオール<br>
-<br>
-スイスの首都は？<br>
-<input type="radio" name="Q2">ジュネーブ<br>
-<input type="radio" name="Q2">チューリッヒ<br>
-<input type="radio" name="Q2">ベルン<br>
-<br>
-ドイツの首都は？<br>
-<input type="radio" name="Q3">ハンブルク<br>
-<input type="radio" name="Q3">ブレーメン<br>
-<input type="radio" name="Q3">ベルリン<br>
-<br>
-スペインの首都は？<br>
-<input type="radio" name="Q4">バルセロナ<br>
-<input type="radio" name="Q4">マドリード<br>
-<input type="radio" name="Q4">リスボン<br>
-<br>
-オーストラリアの首都は？<br>
-<input type="radio" name="Q5">シドニー<br>
-<input type="radio" name="Q5">メルボルン<br>
-<input type="radio" name="Q5">キャンベラ<br>
-<br>
-<input type="button" value="採点" onclick="saiten()" />
+<?php
+$city = [
+  "オタワ",
+  "トロント",
+  "モントリオール",
+  "ジュネーブ",
+  "チューリッヒ",
+  "ベルン",
+  "ハンブルク",
+  "ブレーメン",
+  "ベルリン",
+  "バルセロナ",
+  "マドリード",
+  "リスボン",
+  "シドニー",
+  "メルボルン",
+  "キャンベラ"
+];
+
+$country = [
+  "カナダ",
+  "スイス",
+  "ドイツ",
+  "スペイン",
+  "オーストラリア"
+];
+
+//この配列をループして問題を作る
+//3回に1回は問題番号が変わる。国名も変わる。
+//剰余演算子を使う
+
+$c = 0;
+
+foreach ($city as $key => $value) {
+  if($key % 3 == 0) 
+    echo "<br>",$country[$c++],"の首都は？<br>";
+  echo "<input type='radio' name='Q$c' value='$key'>$value<br>";
+}
+?>
+<input type="submit" name="submit" value="php採点">
 </form>
 
-
-
-<script>
-function saiten(){
-  var seikai=0; //正解数を入れる変数
-  //答えの番号を配列に入れる
-  var trueAns = new Array(0,5,8,10,14);
-  //正解のラジオボタンがチェックされているか確認
-  for (i=0 ; i<5 ; i++)  {
-    if( document.radioB.elements[trueAns[i]].checked )
-      seikai++;
+<?php
+if( isset($_GET['submit']) ) {
+  //採点ボタンを押していたら
+  $seikai = 0;
+  $trueAns = [0,5,8,10,14];
+ 
+  for ($i=0; $i<5; $i++) {
+    $q = $i+1 ; //Qは1から始まるので
+    if( isset($_GET["Q$q"] ) &&
+    $_GET["Q$q"] == $trueAns[$i]) //未回答でエラーにならないように
+    $seikai++;
   }
-  
-  alert("あなたは"+seikai*20+"点でした！");
+  echo "あなたは",$seikai*20,"点でした！";
 }
-</script>
+?>
+
